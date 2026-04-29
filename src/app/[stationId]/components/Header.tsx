5943932dad60c55b54c7ed6d1d2ffd7dff798f2b
Dan L <dan784@gmail.com>
2026-04-29 20:15:05 +0300

    added qr code

--- src/app/[stationId]/components/Header.tsx
+++ src/app/[stationId]/components/Header.tsx
@@ -1,30 +1,34 @@
 import Live from "@/src/components/Live";
 import { Station, UserProfile } from "@/src/lib/types";
+import { ReactQRCode } from '@lglab/react-qr-code'
 
 export default function Header({ stationData, profile, isLive, hostName }: { stationData: Station | null, profile: UserProfile | null, isLive: boolean, hostName: string | null }) {
 
     return (
-        <div className="md:col-span-12 flex flex-col gap-0 items-start">
-            {stationData && (
-                <>
-                    <h1 className="text-4xl font-bold">Tuned in to: {stationData.stationId}</h1>
-                    <div className="flex items-baseline gap-3">
-                        <Live isLive={isLive} />
-                        {stationData.hostId === profile?.uid ? (
-                            <p className="text-slate-400">You are hosting this station</p>
-                        ) : (
-                            <p className="text-slate-400">Hosted by {hostName ?? stationData.hostId}</p>
-                        )}
-                    </div>
-                    <div className="mt-2 text-slate-600">
-                        {stationData.isPlaying ? (
-                            <span className="font-medium">Now Playing: {stationData.videoTitle || "Nothing yet..."}</span>
-                        ) : (
-                            <span className="font-medium">Station is paused</span>
-                        )}
-                    </div>
-                </>
-            )}
+        <div className="md:col-span-12 flex ">
+            <ReactQRCode value={window.location.href} marginSize={0} />
+            <div className="flex flex-col gap-0 items-start">
+                {stationData && (
+                    <>
+                        <h1 className="text-4xl font-bold">Tuned in to: {stationData.stationId}</h1>
+                        <div className="flex items-baseline gap-3">
+                            <Live isLive={isLive} />
+                            {stationData.hostId === profile?.uid ? (
+                                <p className="text-slate-400">You are hosting this station</p>
+                            ) : (
+                                <p className="text-slate-400">Hosted by {hostName ?? stationData.hostId}</p>
+                            )}
+                        </div>
+                        <div className="mt-2 text-slate-600">
+                            {stationData.isPlaying ? (
+                                <span className="font-medium">Now Playing: {stationData.videoTitle || "Nothing yet..."}</span>
+                            ) : (
+                                <span className="font-medium">Station is paused</span>
+                            )}
+                        </div>
+                    </>
+                )}
+            </div>
         </div>
     )
 }
