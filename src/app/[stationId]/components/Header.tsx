2daee1e593d14455e0edb3aa5a7301c268e6cc48
Dan L <dan784@gmail.com>
2026-04-29 20:21:20 +0300

    ui fix

--- src/app/[stationId]/components/Header.tsx
+++ src/app/[stationId]/components/Header.tsx
@@ -5,13 +5,17 @@ import { ReactQRCode } from '@lglab/react-qr-code'
 export default function Header({ stationData, profile, isLive, hostName }: { stationData: Station | null, profile: UserProfile | null, isLive: boolean, hostName: string | null }) {
 
     return (
-        <div className="md:col-span-12 flex ">
-            <ReactQRCode value={window.location.href} marginSize={0} />
+        <div className="md:col-span-12 flex flex-col md:flex-row gap-6">
+
+            <div className="hidden md:block">
+                <ReactQRCode value={window.location.href} marginSize={0} />
+            </div>
+
             <div className="flex flex-col gap-0 items-start">
                 {stationData && (
                     <>
                         <h1 className="text-4xl font-bold">Tuned in to: {stationData.stationId}</h1>
-                        <div className="flex items-baseline gap-3">
+                        <div className="flex items-center gap-3 mt-4">
                             <Live isLive={isLive} />
                             {stationData.hostId === profile?.uid ? (
                                 <p className="text-slate-400">You are hosting this station</p>
